78357d9effaa16f127e22c5048ffe214ceb64f4f
michsky119-glitch <michaelholovsky119@gmail.com>
2026-02-12 19:53:46 +0100

    feat: add docker and docker-compose for backend and frontend

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
-const db = new Database(path.join(__dirname, 'todo.db'));
+const dataDir = path.join(__dirname, 'data');
+fs.mkdirSync(dataDir, { recursive: true });
+const db = new Database(path.join(dataDir, 'todo.db'));
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS tasks (
